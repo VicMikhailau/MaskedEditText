bd8993caa78a990ed4929723ee5a5fddb02bae0b
vicmikhailau <vicmikhailau@gmail.com>
2024-11-08 13:34:00 +0300

    [*] Updated AGP.

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+include(":app", ":MaskedEditText")
